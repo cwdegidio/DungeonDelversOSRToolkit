f1926efe43546ea9cbc149f068b2d09a591ba2f5
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-11-14 23:19:07 -0600

    Initial commit

---
+++ DungeonDelversOSRToolkit/DungeonDelversOSRToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDelversOSRToolkitApp.swift
+//  DungeonDelversOSRToolkit
+//
+//  Created by Curtis DeGidio on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDelversOSRToolkitApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
