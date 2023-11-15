11c86ad17d238632b4d12ce0231f2c6c3c4ce771
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-11-15 00:33:09 -0600

    Added Handlee font

--- DungeonDelversOSRToolkit/ContentView.swift
+++ DungeonDelversOSRToolkit/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+  init() {
+    for family in UIFont.familyNames {
+      print(family)
+      for fontName in UIFont.fontNames(forFamilyName: family) {
+        print("-- \(fontName)")
+      }
+    }
+  }
+
   var body: some View {
     VStack {
       Image(systemName: "globe")
         .imageScale(.large)
         .foregroundStyle(.tint)
+      Text("18")
+        .font(Font.custom("Handlee-Regular", size: 72))
       Text("Hello, world!")
+        .font(Font.custom("Courier", size: 36))
+        .bold()
     }
     .padding()
   }
